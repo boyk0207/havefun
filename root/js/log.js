b3fbe27b89c0d9bd18345130d9454433139c687a
buynaa <buynaa.02071@gmail.com>
2026-03-03 02:40:14 +0800

    log in index.html ruu handdag baihar saijruulav

--- root/js/log.js
+++ root/js/log.js
@@ -5,6 +5,9 @@ const registerBtn = document.getElementById("register");
 const loginBtn = document.getElementById("login");
 const getStartedBtn = document.getElementById("getStartedBtn");
 
+const signInForm = document.querySelector(".sign-in form");
+const signUpForm = document.querySelector(".sign-up form");
+
 function openPopup(signUp = false) {
   container.classList.add("show");
   overlay.classList.add("show");
@@ -28,3 +31,21 @@ overlay.addEventListener("click", closePopup);
 // Toggle animation buttons
 registerBtn.addEventListener("click", () => container.classList.add("active"));
 loginBtn.addEventListener("click", () => container.classList.remove("active"));
+
+
+/* =========================
+   LOGIN → INDEX.HTML
+========================= */
+const loginForm = document.getElementById("loginForm");
+const registerForm = document.getElementById("registerForm");
+
+loginForm.addEventListener("submit", function(e){
+  e.preventDefault();
+  window.location.href = "index.html";
+});
+
+registerForm.addEventListener("submit", function(e){
+  e.preventDefault();
+  window.location.href = "index.html";
+});
+
